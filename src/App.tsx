9fbdbb63dcb54a1748cf19599fa3328691f1a1f8
Antonio <antonio.gaming10@gmail.com>
2022-11-05 00:46:54 +0100

    Added upgrades

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./app.css"
+import UpgradeState from "./classes/upgradeState";
+import Button from "./components/button";
 import { ClickHandler } from "./components/clickHandler";
 import { DisplayStats } from "./components/displayStats";
 
 export function App(){
   const [balance, setBalance] = useState(0);
 
+  const upgradeMap = useRef(new Map<string, UpgradeState>([
+    ['clickUpgrade', new UpgradeState(15, 1.1, 1, 0.1)],
+    ['autoClicker01', new UpgradeState(20, 1.2, 0, 0.1)]
+  ]))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('Attempting to invoke autoClicker components.');
+      setBalance(Math.round((balance + 
+        upgradeMap.current.get('autoClicker01')!.increment) * 10) / 10)
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
   return(
     <>
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm">
-          <ClickHandler balance = {balance} setBalance = {setBalance} />
-          <DisplayStats balance = {balance}/>
-        </div>
-        <div className="col-sm">
-          <h1>Upgrades</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm">
+            <ClickHandler 
+            balance = {balance} 
+            setBalance = {setBalance} 
+            increment= {upgradeMap.current.get('clickUpgrade')!.increment}/>
+            <DisplayStats balance = {balance}/>
+          </div>
+          <div className="col-sm">
+            <h1>Upgrades</h1>
+            <Button
+            id="clickUpgrade"
+            text="Main upgrade"
+            clickHandler={(id) => {upgradeInvocationHandler(id,balance,setBalance,upgradeMap);}}
+            /> <br/>
+            <Button
+            id="autoClicker01"
+            text="Auto Clicker 1"
+            clickHandler={(id) => {upgradeInvocationHandler(id,balance,setBalance,upgradeMap);}}
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
+}
+
+const upgradeInvocationHandler = (
+  id: string,
+  balance: number,
+  setBalance: React.Dispatch<React.SetStateAction<number>>,
+  upgradeMap: React.MutableRefObject<Map<string, UpgradeState>>
+) : void => {
+  if (!upgradeMap.current.has(id)) {
+    return;
+  }
+
+  const cost = upgradeMap.current.get(id)!.currentCost;
+
+  if (upgradeMap.current.get(id)!.upgrade(balance)) {
+    console.log(`Upgraded ${id} component.`);
+    setBalance(balance - cost);
+  } else {
+    console.log(`Balance is too low to upgrade ${id} component.`)
+  }
 }
