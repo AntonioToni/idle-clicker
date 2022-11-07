e57ffe7e6849ca1f6be0bbb8c2bfb9d1abf2ab00
Antonio <antonio.gaming10@gmail.com>
2022-11-07 02:00:15 +0100

    Added thousands separator comma

--- src/components/displayStats.tsx
+++ src/components/displayStats.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 export function DisplayStats(props : {balance : number, clickIncrement : number, autoIncrement: number}){
+  function addcomma(x:any) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
   return(
     <>
-      <h1>Balance: {Math.trunc(props.balance)}</h1>
+      <h1>Balance: {addcomma(Math.trunc(props.balance))}</h1>
       <h2>Balance per click: {props.clickIncrement}</h2>
       <h2>Balance per second: {props.autoIncrement}</h2>
     </>
