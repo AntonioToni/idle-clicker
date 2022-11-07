c52440be9bc7c5ac9e63bc97eb07d581ac845a43
Antonio <antonio.gaming10@gmail.com>
2022-11-07 01:47:17 +0100

    Decimals on balance no longer shown

--- src/components/displayStats.tsx
+++ src/components/displayStats.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function DisplayStats(props : {balance : number, clickIncrement : number, autoIncrement: number}){
   return(
     <>
-      <h1>Balance: {props.balance}</h1>
+      <h1>Balance: {Math.trunc(props.balance)}</h1>
       <h2>Balance per click: {props.clickIncrement}</h2>
       <h2>Balance per second: {props.autoIncrement}</h2>
     </>
