d382a5ec09766a4c4eeac811b2dfda8c32f0d8bb
Dario <dariomrkonjic1@gmail.com>
2022-11-12 02:30:01 +0100

    Fixed issue #4 (#9)
    * Fixed issue #4
    
    * Fixed UI lag on displayStats.
    
    Co-authored-by: AntonioToni <antonio.gaming10@gmail.com>

--- src/components/clickHandler.tsx
+++ src/components/clickHandler.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import logo from '../assets/ClickerLogo.png';
-export function ClickHandler(props: {setBalance : any, balance : number, increment: number}) {
+export function ClickHandler(props: {
+  balanceRef: React.MutableRefObject<{value: number;}>,
+  increment: number
+}) {
 
   function handleClick() {
-    props.setBalance(Math.round((props.balance + props.increment) * 100 ) / 100);
+    /*
+      Should not be using setBalance directly since 
+      it would force a re-render of the parent component (<App>)!
+    */
+    props.balanceRef.current.value = Math.round((props.balanceRef.current.value + props.increment) * 100 ) / 100;
   }
 
-  return(
+  return (
     <>
       <img onClick={handleClick} src={logo} alt="logo" className='logoImg' title='Click me!' />
     </>
