6edec3eba822dde484bcf2fb5cfa34b90cf755e5
Antonio <antonio.gaming10@gmail.com>
2022-11-18 01:45:26 +0100

    Added wipe save confirmation popup

--- src/components/saveGame.tsx
+++ src/components/saveGame.tsx
@@ -1,6 +1,19 @@
-import { Button } from '@mui/material';
+import { Button, Box, Typography, Modal } from '@mui/material';
 import UpgradeState from "../classes/upgradeState";
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 350,
+  bgcolor: 'rgb(14, 16, 17)',
+  border: '2px solid rgb(141, 130, 114)',
+  boxShadow: 24,
+  p: 3,
+};
+
 export function SaveGame(props: {
   balanceRef: React.MutableRefObject<{value: number;}>,
   upgradeMap: React.MutableRefObject<Map<string, UpgradeState>>,
@@ -26,13 +39,13 @@ export function SaveGame(props: {
     handleLoad()
   }, []);
   /*
-    Game is autosaved every 1 minute and 50 seconds to increase
-    time change 2000 to a higher number, to decrease time between
-    saves change 2000 to a lower number.
+    Game is autosaved every 1 minute to increase
+    time change 1400 to a higher number, to decrease time between
+    saves change 1400 to a lower number.
   */
   const counter = useRef({ value: 0})
   counter.current.value+=1;
-  if (counter.current.value >= 2000) {
+  if (counter.current.value >= 1400) {
     handleSave();
     counter.current.value=0;
     //TODO ADD popup saying game saved
@@ -49,14 +62,36 @@ export function SaveGame(props: {
     localStorage.removeItem("AC2Level");
     localStorage.removeItem("AC3Level");
     localStorage.removeItem("AC4Level");
-    console.log("Game wiped")
+    console.log("Game wiped");
+    handleClose();
     //TODO add confirmation popup
   }
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return(
     <>
-      <Button onClick={handleSave} style={{margin: "10px"}} variant="contained">Save</Button>
-      <Button onClick={handleLoad} style={{margin: "10px"}} variant="contained">Load</Button> <br/>
-      <Button onClick={wipeSave} size="small" style={{margin: "10px"}} variant="contained" color="error">Wipe save</Button>
+      <Button onClick={handleOpen} size="small" style={{margin: "10px"}} variant="contained" color="error">Wipe save</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            WARNING
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Do you REALLY want to wipe your save?
+          </Typography>
+          <Typography variant='caption'>
+            You will lose your progress, there is no going back!
+          </Typography>
+          <Button onClick={wipeSave}>Yes</Button>
+          <Button onClick={handleClose}>No</Button>
+        </Box>
+      </Modal>
     </>
   )
 }
