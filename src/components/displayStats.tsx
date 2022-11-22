f37d0b618b03a0ec662bc1d40daa3a7e2a0b3054
Antonio <antonio.gaming10@gmail.com>
2022-11-22 03:43:00 +0100

    Metric prefix for numbers greater then 999,999

--- src/components/displayStats.tsx
+++ src/components/displayStats.tsx
@@ -10,6 +10,16 @@ export function DisplayStats(props : {
   function addcomma(x:any) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
   }
+  const formatNumber = (n: number) => {
+    if (n < 1e6) return n;
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "G";
+    if (n >= 1e12 && n < 1e15) return +(n / 1e12).toFixed(1) + "T";
+    if (n >= 1e15 && n < 1e18) return +(n / 1e15).toFixed(1) + "P";
+    if (n >= 1e18 && n < 1e21) return +(n / 1e18).toFixed(1) + "E";
+    if (n >= 1e21 && n < 1e24) return +(n / 1e21).toFixed(1) + "Z";
+    if (n >= 1e24) return +(n / 1e24).toFixed(1) + "Y";
+  };
   /*
     displayBalance and setDisplayBalance is used to keep
     displayed balanced updated every 10ms so user doesn't feel the UI lag.
@@ -27,9 +37,9 @@ export function DisplayStats(props : {
 
   return(
     <div className="stats">
-      <h1>Balance: {addcomma(Math.trunc(displayBalance))}</h1>
+      <h1>Balance: {addcomma(formatNumber(Math.trunc(displayBalance)))}</h1>
       <h4>per click: {props.clickIncrement}</h4>
-      <h4>per second: {props.autoIncrement}</h4>
+      <h4>per second: {addcomma(formatNumber(props.autoIncrement))}</h4>
     </div>
   )
 }
